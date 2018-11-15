8d16f7358a8dc89a9c7e28fd07a3c375f54aff69
Jorge Quesada <joarkemu@gmail.com>
2018-11-15 14:39:16 -0500

    2018-11-15: Commit con mejoras de observaciones en facturas y cuadros de busquda para todas las grillas

--- syncfusion_payc/syncfusion_payc/Models/contabilidad.Designer.cs
+++ syncfusion_payc/syncfusion_payc/Models/contabilidad.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'E:\REPO_TEMP\syncfusion_payc\syncfusion_payc\Models\contabilidad.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\REPOSITORIO\syncfusion_payc\syncfusion_payc\Models\contabilidad.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
